63142f30202439d379596a5f8e78d6a2f89658b1
betelhem16 <betelhem.kassaye@aastustudent.edu.et>
2025-08-28 02:53:43 +0300

    Update models.py

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)      # Required by ALX
-    author = models.CharField(max_length=100)     # Required by ALX
-    publication_year = models.IntegerField()      # Required by ALX
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
 
 
